30dc3d7245d477f7c817273e5a91a82ce6548cca
Edvinas82 <ziukoslenis@gmail.com>
2023-02-14 17:41:40 +0200

    Sukurtas projektas

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        isvestiPenkisKartus('a');
+
+    char c = 'b';
+
+    isvestiPenkisKartus(c);
+    }
+    public static void isvestiPenkisKartus(char raide){
+        System.out.println(raide);
+        System.out.println(raide);
+        System.out.println(raide);
+        System.out.println(raide);
+        System.out.println(raide);
+    }
+}
